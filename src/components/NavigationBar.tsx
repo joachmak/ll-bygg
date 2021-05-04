657f4f3faea062909300ba05a811db92ccb71f32
Joachmak <joachmak@users.noreply.github.com>
2021-05-04 13:38:10 +0200

    Add navbar backdrop

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,5 +1,6 @@
-import {AppBar, createStyles, IconButton, makeStyles, Theme, Toolbar} from "@material-ui/core";
+import {AppBar, Backdrop, createStyles, IconButton, makeStyles, Theme, Toolbar} from "@material-ui/core";
 import MenuIcon from '@material-ui/icons/Menu';
+import {useState} from "react";
 
 
 const useStyles = makeStyles((theme: Theme) =>
@@ -20,23 +21,34 @@ const useStyles = makeStyles((theme: Theme) =>
             borderRadius: 5,
             marginTop: 10,
 
-        }
+        },
+        backdrop: {
+            zIndex: theme.zIndex.drawer + 1,
+            color: '#fff',
+        },
     }),
 );
 
 export default function NavigationBar() {
     const classes = useStyles();
+    const [openBackdrop, setOpenBackdrop] = useState(false);
+    const handleToggle = () => {
+        setOpenBackdrop(!openBackdrop);
+    }
     return (
         <>
             <AppBar className={classes.appbar} position="static">
                 <Toolbar>
                     <div className={classes.menuIconBox}>
-                        <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                        <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu" onClick={handleToggle}>
                                 <MenuIcon />
                         </IconButton>
                     </div>
                 </Toolbar>
             </AppBar>
+            <Backdrop className={classes.backdrop} open={openBackdrop} onClick={handleToggle}>
+                Test
+            </Backdrop>
         </>
     )
 }
