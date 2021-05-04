9d7595e78ec9a60b5a7ccf391e1b23d8c4617a4f
Joachmak <joachmak@users.noreply.github.com>
2021-05-04 14:05:11 +0200

    Add header bg with vignette

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -3,6 +3,7 @@ import MenuIcon from '@material-ui/icons/Menu';
 import {useState} from "react";
 
 
+const menuIconSize = 40;
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
         menuButton: {
@@ -11,13 +12,14 @@ const useStyles = makeStyles((theme: Theme) =>
         appbar: {
             background: "transparent",
             boxShadow: "none",
+            position: "static",
+            height: 0,
         },
         menuIconBox: {
             backgroundColor: "orange",
             display: "flex",
             justifyContent: "center",
             alignItems: "center",
-            padding: 5,
             borderRadius: 5,
             marginTop: 10,
 
@@ -25,6 +27,11 @@ const useStyles = makeStyles((theme: Theme) =>
         backdrop: {
             zIndex: theme.zIndex.drawer + 1,
             color: '#fff',
+            backgroundColor: "rgba(255,102,0,0.8)",
+        },
+        icon: {
+            width: menuIconSize,
+            height: menuIconSize,
         },
     }),
 );
@@ -32,22 +39,22 @@ const useStyles = makeStyles((theme: Theme) =>
 export default function NavigationBar() {
     const classes = useStyles();
     const [openBackdrop, setOpenBackdrop] = useState(false);
-    const handleToggle = () => {
+    const handleToggle = () => { // Toggle backdrop
         setOpenBackdrop(!openBackdrop);
     }
     return (
         <>
-            <AppBar className={classes.appbar} position="static">
+            <AppBar className={classes.appbar}>
                 <Toolbar>
                     <div className={classes.menuIconBox}>
                         <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu" onClick={handleToggle}>
-                                <MenuIcon />
+                                <MenuIcon className={classes.icon} />
                         </IconButton>
                     </div>
                 </Toolbar>
             </AppBar>
             <Backdrop className={classes.backdrop} open={openBackdrop} onClick={handleToggle}>
-                Test
+                <h1>MENYKNAPPER VIL VISES HER</h1>
             </Backdrop>
         </>
     )
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,8 +1,25 @@
+import {createStyles, makeStyles, Theme} from "@material-ui/core";
+//import headerImage from "./header.JPG"
+
+let img = require("./header.JPG")
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        header: {
+            height: "100vh",
+
+            backgroundImage: "url('" + img.default + "')",
+            backgroundSize: "cover",
+            // @ts-ignore
+            boxShadow: "inset 0 0 7em 1em #000",
+        },
+    }),
+);
 
 export default function Header() {
+    const classes = useStyles()
     return (
-        <div>
-            
+        <div className={classes.header}>
         </div>
     )
 }
