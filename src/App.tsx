02a5792cc31f021905f00150e307792af59327c0
Joachim Maksim <joachim.maksim@ntnu.no>
2021-05-15 21:58:19 +0200

    Add about-section

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Header from "./components/header/Header";
 import NavigationBar from "./components/NavigationBar";
+import About from "./components/about/About";
 
 function App() {
   return (
     <div className="App">
         <NavigationBar />
         <Header />
+        <About />
     </div>
   );
 }
---
+++ src/components/about/About.tsx
@@ -0,0 +1,42 @@
+import {Container, createStyles, Grid, makeStyles, Theme, Typography} from "@material-ui/core";
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        container: {
+            display: "flex",
+            justifyContent: "center",
+            margin: "50px auto",
+            textAlign: "left",
+        },
+        title: {
+            marginBottom: 15,
+        },
+        text: {
+            lineHeight: "200%",
+        }
+    }),
+);
+export default function About() {
+    const classes = useStyles()
+    return (
+        <>
+            <Container className={classes.container}>
+                <Grid item lg={8} md={10} xs={12}>
+                    <Typography variant={"h4"} className={classes.title}>
+                        Om oss
+                    </Typography>
+                    <Typography variant={"caption"} color={"textSecondary"} className={classes.title}>
+                        <b>STIFTET I 2019</b>
+                    </Typography>
+                    <Typography variant={"body2"} color={"textSecondary"} className={classes.text}>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi commodi dignissimos error fugit iste labore, maxime necessitatibus vero! Consectetur, dignissimos fugit id ipsam iure laboriosam non quas quibusdam veritatis voluptate?
+                        <br /><br />
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus cumque dolor maxime perspiciatis quia. Ab alias, assumenda, est ex, magnam molestiae mollitia natus numquam quas quasi totam velit vero voluptates?
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum itaque obcaecati optio saepe sequi? Beatae consequuntur corporis deleniti dolorem ex, incidunt libero nobis nulla obcaecati odio sequi, soluta unde vero?
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. A autem beatae dolores eius error esse exercitationem fugit harum in incidunt ipsum, itaque, labore officiis praesentium provident repellendus repudiandae sapiente voluptatem?
+                    </Typography>
+                </Grid>
+            </Container>
+        </>
+    )
+}
