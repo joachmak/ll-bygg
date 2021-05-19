f315062ef897f1308eba2456789060e57a26481d
Joachmak <joachmak@users.noreply.github.com>
2021-05-19 15:53:36 +0200

    Configure carousel settings

--- src/components/projects/ProjectCarousel.tsx
+++ src/components/projects/ProjectCarousel.tsx
@@ -25,9 +25,48 @@ export default function ProjectCarousel(props:carouselProps) {
             {/*@ts-ignore*/}
             <Carousel
                 navButtonsAlwaysVisible
+                fullHeightHover={false}
                 autoPlay={false}
                 NextIcon={<NavigateNextIcon />}
                 PrevIcon={<NavigateBeforeIcon />}
+                /* @ts-ignore */
+                navButtonsProps={{          // Change the colors and radius of the actual buttons. THIS STYLES BOTH BUTTONS
+                    style: {
+                        backgroundColor: 'orange',
+                    }
+                }}
+                buttonWrapper={
+                    {
+                        style: {
+                            position: "absolute",
+
+                            height: "150px",
+                            backgroundColor: "orange",
+                            top: "calc(50% - 70px)",
+                            '&:hover': {
+                                '& $button': {
+                                    backgroundColor: "black",
+                                    filter: "brightness(100%)",
+                                    opacity: "1 !important"
+                                }
+                            }
+                        }
+                    }
+                }
+                button={{
+                    style:{
+                        position: "relative",
+                        backgroundColor: "#494949",
+                        top: "calc(50% - 20px) !important",
+                        color: "white",
+                        fontSize: "30px",
+                        transition: "200ms",
+                        cursor: "pointer",
+                        '&:hover': {
+                            opacity: "1 !important"
+                        },
+                    }
+                }}
             >
             {
                 props.imgUrls.map(imageUrl => <img alt={"bilde"} className={classes.image} src={imageUrl} />)
