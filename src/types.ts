84561f123704ffa14486eeb6447d19a463a687b7
Joachim Maksim <joachim.maksim@ntnu.no>
2021-05-22 17:42:59 +0200

    Add types file

---
+++ src/types.ts
@@ -0,0 +1,31 @@
+
+type Employee = {
+    name:string,
+    role:string,
+    description:string,
+    priority:number,
+}
+
+type Project = {
+    description:string,
+    images:string[],
+    thumbnail: string,
+    title:string,
+}
+
+type AboutSection = {
+    description:string,
+    title:string,
+}
+
+type HeaderSection = {
+    imgUrl:string,
+}
+
+type ServicesSection = {
+    byggDesc: string,
+    monteringDesc: string,
+    rehabiliteringDesc: string,
+}
+
+export type {Employee, Project, AboutSection, HeaderSection, ServicesSection}
