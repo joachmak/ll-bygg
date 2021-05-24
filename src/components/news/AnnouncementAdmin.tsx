4f5730a709294638a430665c3ab1845d160e13b1
Joachim Maksim <joachim.maksim@ntnu.no>
2021-05-24 02:04:33 +0200

    Create init UI for news admin section

--- src/components/news/AnnouncementAdmin.tsx
+++ src/components/news/AnnouncementAdmin.tsx
@@ -1,4 +1,4 @@
-import {createStyles, Grid, makeStyles, Theme, Typography} from "@material-ui/core";
+import {Button, createStyles, Grid, makeStyles, TextField, Theme, Typography} from "@material-ui/core";
 import {NewsDoc} from "../../types";
 import {Doc} from "typesaurus";
 
@@ -12,37 +12,44 @@ interface announcementInterface {
 export default function Announcement(props:{announcement:Doc<NewsDoc>}) {
     const useStyles = makeStyles((theme: Theme) =>
         createStyles({
-            title: {
-                marginBottom: 5,
+            txtField: {
+                marginBottom: 15,
             },
-            text: {
-                lineHeight: "200%",
+            root: {
+                //border: "1px solid orange",
+                padding: "15px 15px 0",
+                margin: "15px 0 ",
+                borderRadius: 10,
+                boxShadow: "0 3px 5px rgba(0,0,0,0.2)"
             },
-            dateString: {
-                textTransform: "capitalize",
+            btn: {
+                marginBottom: 15,
+                marginRight: 10,
             }
         }),
     );
     const classes = useStyles()
     return (
         <>
-            <Grid item xs={12}>
-                <Typography variant={"h5"} className={classes.title}>
-                    {props.announcement.data.title}
-                </Typography>
-                <Typography variant={"caption"} color={"textSecondary"} className={classes.dateString}>
-                    <b>{props.announcement.data.datetime.toLocaleDateString("no-NB", {
-                        weekday: 'short', // long, short, narrow
-                        day: 'numeric', // numeric, 2-digit
-                        year: 'numeric', // numeric, 2-digit
-                        month: 'long', // numeric, 2-digit, long, short, narrow
-                        hour: 'numeric', // numeric, 2-digit
-                        minute: 'numeric', // numeric, 2-digit
-                    })}</b>
-                </Typography>
-                <Typography variant={"body2"} color={"textSecondary"} className={classes.text}>
-                    {props.announcement.data.description}
-                </Typography>
+            <Grid className={classes.root} item xs={12}>
+                <TextField
+                    className={classes.txtField}
+                    fullWidth
+                    multiline
+                    variant={"outlined"}
+                    label={"Tittel"}
+                    defaultValue={props.announcement.data.title}
+                />
+                <TextField
+                    className={classes.txtField}
+                    fullWidth
+                    multiline
+                    variant={"outlined"}
+                    label={"Beskrivelse"}
+                    defaultValue={props.announcement.data.description}
+                />
+                <Button className={classes.btn} variant={"outlined"} color={"primary"}>Oppdater</Button>
+                <Button className={classes.btn} variant={"outlined"} color={"secondary"}>Slett</Button>
             </Grid>
         </>
     )
