f7edd3952b8316155dac850ae20729d724b885da
Joachmak <joachmak@users.noreply.github.com>
2021-05-25 16:51:38 +0200

    Enable admins to delete projects

--- src/components/projects/ProjectAdminCard.tsx
+++ src/components/projects/ProjectAdminCard.tsx
@@ -1,10 +1,11 @@
 import {Backdrop, Button, createStyles, Grid, Link, makeStyles, TextField, Theme, Typography} from "@material-ui/core";
 import {Project} from "../../types";
-import {collection, Doc, update} from "typesaurus";
+import {collection, Doc, remove, update} from "typesaurus";
 import {useState} from "react";
 import ProjectAdminImage from "./ProjectAdminImage";
 import {Add, Backspace, Delete, Update} from "@material-ui/icons";
 import ProjectCarousel from "./ProjectCarousel";
+import ConfirmDeleteDialog from "../ConfirmDeleteDialog";
 
 export default function ProjectAdminCard(props:{project:Doc<Project>}) {
     const useStyles = makeStyles((theme: Theme) =>
@@ -61,6 +62,7 @@ export default function ProjectAdminCard(props:{project:Doc<Project>}) {
     const [open, setOpen] = useState(false)
     const [rerender, setRerender] = useState(false)
     const [isProcessing, setIsProcessing] = useState(false)
+    const [openDialog, setOpenDialog] = useState(false)
     const setImageByID = (id:number, value:string) => {
         let res = []
         for (let i = 0; i < images.length; i++) {
@@ -90,6 +92,19 @@ export default function ProjectAdminCard(props:{project:Doc<Project>}) {
         update(projectCollection, props.project.ref.id, {title: title, description: description, thumbnail: thumbnailUrl, images:images})
             .then(() => {
                 alert("Prosjektet har blitt oppdatert!")
+                setIsProcessing(false)
+            })
+            .catch((e) => {
+                alert(e)
+                console.error(e)
+                setIsProcessing(false)
+            })
+    }
+    const deleteProject = () => {
+        setIsProcessing(true)
+        remove(projectCollection, props.project.ref.id)
+            .then(() => {
+                alert("Prosjektet har blitt slettet!")
                 setIsProcessing(false)
             })
             .catch((e) => {
@@ -190,9 +205,20 @@ export default function ProjectAdminCard(props:{project:Doc<Project>}) {
                     variant={"contained"}
                     color={"secondary"}
                     disabled={isProcessing}
+                    onClick={() => {
+                        setOpenDialog(true)
+                    }}
                 >
                     Slett
                 </Button>
+                <ConfirmDeleteDialog
+                    key={props.project.ref.id}
+                    setIsOpen={setOpenDialog}
+                    isOpen={openDialog}
+                    title={"Er du sikker på at du vil slette prosjektet?"}
+                    information={"Prosjektet det gjelder: " + props.project.data.title}
+                    deleteFunc={deleteProject}
+                />
                 <Backdrop className={classes.backdrop} open={open} onClick={() => {setOpen(false)}}>
                     <ProjectCarousel imgUrls={images} />
                 </Backdrop>
