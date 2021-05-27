eb5e1b472d7bb84ce7e68d46638c6432bf4f6f88
Joachim Maksim <joachim.maksim@ntnu.no>
2021-05-27 13:59:57 +0200

    Improve jallacaptcha

--- src/components/contactform/ContactForm.tsx
+++ src/components/contactform/ContactForm.tsx
@@ -31,15 +31,13 @@ export default function ContactForm(props: {margin:number}) {
                         Kontakt oss
                     </Typography>
                     <form method={"post"} noValidate action="https://formspree.io/f/xnqldjry" autoComplete="off">
-                        <TextField type={"email"} name={"_replyto"} required fullWidth id="standard-basic" label="E-post" className={classes.textField} />
-                        <TextField required fullWidth id="standard-basic" label="Navn" className={classes.textField} />
-                        <TextField required fullWidth id="standard-basic" label="Adresse" className={classes.textField} />
-                        <TextField required fullWidth id="standard-basic" label="Tema" className={classes.textField} />
-                        <TextField name={"message"} required multiline rows={5} fullWidth id="standard-basic" label="Melding" className={classes.textField} />
-                        <TextField value={notRobot} onChange={(e) => setNotRobot(e.target.value)} required fullWidth id="standard-basic" label="Vennligst skriv 'ikkerobot' for å bekrefte at du er en ekte person" className={classes.textField} />
-                        <Button disabled={notRobot !== "ikkerobot"} type={"submit"} variant={"outlined"} color={"primary"} >Send henvendelse</Button>
-
-
+                        <TextField type={"email"} name={"_replyto"} required fullWidth id="standard-basic" label="E-post" className={classes.textField} /> {/* TODO: Enforce this */}
+                        <TextField fullWidth id="standard-basic" label="Navn" className={classes.textField} />
+                        <TextField fullWidth id="standard-basic" label="Adresse" className={classes.textField} />
+                        <TextField fullWidth id="standard-basic" label="Tema" className={classes.textField} />
+                        <TextField name={"message"} multiline rows={5} fullWidth id="standard-basic" label="Melding" className={classes.textField} />
+                        <TextField value={notRobot} onChange={(e) => setNotRobot(e.target.value)} fullWidth id="standard-basic" label="Vennligst skriv 'ikkerobot' for å bekrefte at du er en ekte person" className={classes.textField} />
+                        <Button disabled={notRobot !== "ikkerobot" && notRobot !== "'ikkerobot'" && notRobot !== '"ikkerobot"'} type={"submit"} variant={"outlined"} color={"primary"} >Send henvendelse</Button>
                     </form>
                 </Grid>
             </Container>
