3d8da6f249bc7f2cc1d3a0a541e2e86327d7a92e
Joachim Maksim <joachim.maksim@ntnu.no>
2021-05-29 13:23:23 +0200

    Updates

--- src/adminUtils.ts
+++
@@ -1,9 +0,0 @@
-
-const validateImage = async (url:string, errorFunc:() => any, successFunc:() => any) => {
-    let img = new Image()
-    img.onerror = () => errorFunc()
-    img.onload = () => successFunc()
-    img.src = url
-}
-
-export {validateImage}
---
+++ static/js/3.7c93c87d.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpll-bygg"]=this["webpackJsonpll-bygg"]||[]).push([[3],{245:function(s,p,t){"use strict";t.r(p);t(134)}}]);
+//# sourceMappingURL=3.7c93c87d.chunk.js.map
