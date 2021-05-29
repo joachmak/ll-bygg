f00d956e6fb2381d5eb50330cd6e33c73ba0d6d7
Joachim Maksim <joachim.maksim@ntnu.no>
2021-05-29 10:50:04 +0200

    Fix firefox layout bug

--- src/components/news/NewsAdmin.tsx
+++ src/components/news/NewsAdmin.tsx
@@ -2,19 +2,22 @@ import {Doc} from "typesaurus";
 import {NewsDoc} from "../../types";
 import AnnouncementAdmin from "./AnnouncementAdmin";
 import CreateAnnouncementAdmin from "./CreateAnnouncementAdmin";
-import {Typography} from "@material-ui/core";
+import {Grid, Typography} from "@material-ui/core";
 export default function NewsAdmin(props:{existingNewsDocs:Doc<NewsDoc>[]}) {
     return (
         <>
-            <Typography variant={"h5"} color={"textSecondary"}>Publiser nyhet</Typography>
-            <CreateAnnouncementAdmin />
-            <Typography variant={"h5"} color={"textSecondary"}>Rediger nyheter</Typography>
-            {
-                props.existingNewsDocs
-                    // @ts-ignore
-                    .sort((a,b) => b.data.datetime - a.data.datetime)
-                    .map(announcement => <AnnouncementAdmin key={announcement.ref.id} announcement={announcement} />)
-            }
+            <Grid container>
+                <Typography variant={"h5"} color={"textSecondary"}>Publiser nyhet</Typography>
+                <CreateAnnouncementAdmin />
+                <Typography variant={"h5"} color={"textSecondary"}>Rediger nyheter</Typography>
+                <br />
+                {
+                    props.existingNewsDocs
+                        // @ts-ignore
+                        .sort((a,b) => b.data.datetime - a.data.datetime)
+                        .map(announcement => <AnnouncementAdmin key={announcement.ref.id} announcement={announcement} />)
+                }
+            </Grid>
         </>
     )
 }
